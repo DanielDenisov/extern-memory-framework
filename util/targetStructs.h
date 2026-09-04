cb54bcbbdb1d99da857ab91882830ff0b7c6c6fe
Daniel <github@danieldenisov.com>
2026-09-04 18:46:53 -0400

    added examples of structs to read + framework for setting them up

---
+++ util/targetStructs.h
@@ -0,0 +1,19 @@
+/** Convenient for storing functions located inside of the targe process
+ * contains common structs found in C/C++ programs,
+*/
+
+struct TargetString {
+    union {
+        char inline_buffer[16]; 
+        char* heap_pointer;     
+    };
+    size_t length;              
+    size_t capacity;            
+};
+
+template <typename T>
+struct TargetVector {
+    T* start;        
+    T* end;          
+    T* end_capacity; 
+};
